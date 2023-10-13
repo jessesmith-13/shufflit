a92456b68e521cbd257d18687f09ab620ad2879b
Jesse Smith <putamadre@Jesses-MBP.hsd1.ma.comcast.net>
2023-10-13 15:30:50 -0400

    added sass and created initial directories for components and styles

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const LandingPage = () => {
+
+  return (
+    <View>
+      <Text>
+        Hello World
+      </Text>
+    </View>
+  )
+}
+
+export default LandingPage;
