b5314c7ae16fb4d57abc85d867a819b4041fe3c3
Jesse Smith <jesseholden713@gmail.com>
2023-11-09 21:19:24 -0500

    added types for buttons

---
+++ src/components/Buttons/RegularButton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Pressable } from 'react-native';
+import { ButtonProps } from './types'
+
+const RegularButton: React.FunctionComponent<ButtonProps> = (props) => {
+  return (
+    <Pressable>
+    </Pressable>
+  )
+}
