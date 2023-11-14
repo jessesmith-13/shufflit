4febd2202bbc7a6e72a1dc23fd8edeba30285773
Jesse Smith <jesseholden713@gmail.com>
2023-11-14 14:14:01 -0500

    fixed tailwind to include files in app directory

--- app/SignUp.tsx
+++ app/SignUp.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import  { useRouter }  from 'expo-router';
 
 const SignUp = () => {
+  const router = useRouter();
   return (
     <View>
       <Text>
         Sign Up Page
       </Text>
+      <Button
+        onPress={()=> router.back()}
+        title='Go Back'
+      />
     </View>
   )
 }
